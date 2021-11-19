08065f7bd0700a8b3dce9c6953901c3ccaffaeb7
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-11-19 14:08:56 -0800

    updated custom ui

--- homebridge-ui/server.js
+++ homebridge-ui/server.js
@@ -43,6 +43,32 @@ class PluginUiServer extends HomebridgePluginUiServer {
         return []
       }
     })
+    this.onRequest('/getRoombaPassword', async (host) => {
+      var sliceFrom = 13;
+  const packet = 'f005efcc3b2900';
+  var client = tls.connect(8883, host, {timeout: 10000, rejectUnauthorized: false, ciphers: process.env.ROBOT_CIPHERS || 'AES128-SHA256'}, function () {
+    client.write(new Buffer(packet, 'hex'));
+  });
+
+  client.on('data', function (data) {
+    if (data.length === 2) {
+      sliceFrom = 9;
+      return;
+    }
+    if (data.length <= 7) {
+      throw new RequestError('Error getting password. Follow the instructions and try again.');
+//      console.log('Error getting password. Follow the instructions and try again.');
+    } else {
+      return new Buffer(data).slice(sliceFrom).toString();
+      //console.log('Password=> ' + new Buffer(data).slice(sliceFrom).toString() + ' <= Yes, all this string.');
+      //console.log('Use this credentials in dorita980 lib :)');
+    }
+    client.end();
+    process.exit(0);
+  });
+
+  client.setEncoding('utf-8');
+    })
     this.ready()
   }
 }
