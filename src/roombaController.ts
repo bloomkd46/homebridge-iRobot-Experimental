07c4f7733d2c0a65667aa6d41f9b2f9c1699bd2b
bloomkd46 <75853497+bloomkd46@users.noreply.github.com>
2021-11-22 10:08:30 -0800

    fixed too-many-character error

--- src/roombaController.ts
+++ src/roombaController.ts
@@ -5,19 +5,19 @@ var keepAlive = false;
 
 export class roombaController {
   constructor(
-      public readonly log?: Logger,
-        public readonly host?: string,
-        public readonly blid?: string,
-        public readonly password?: string,
-  ){
-    if (host !== null && blid !== null && password !== null){
+    public readonly log?: Logger,
+    public readonly host?: string,
+    public readonly blid?: string,
+    public readonly password?: string,
+  ) {
+    if (host !== null && blid !== null && password !== null) {
       throw Error('No Host/Blid/Password supplied');
     }
   }
 
-  connect(){
-    if(keepAlive){
-      if(roomba === null){
+  connect() {
+    if (keepAlive) {
+      if (roomba === null) {
         roomba = new dorita980.Local(blid, password, host);
         return roomba;
       } else {
@@ -28,67 +28,76 @@ export class roombaController {
       return roomba;
     }
   }
-  getState(){
+  getState() {
     this.connect();
     return roomba
-      .getRobotState(
-        ['batPct', 'mac', 'bin', 'softwareVer', 'lastCommand', 'name', 'cleanMissionStatus', 'carpetBoost', 'vacHigh', 'noAutoPasses', 'twoPass']
-        )
-      .then((state) => {
-        if(!keepAlive) roomba.end();
+      .getRobotState([
+        'batPct',
+        'mac',
+        'bin',
+        'softwareVer',
+        'lastCommand',
+        'name',
+        'cleanMissionStatus',
+        'carpetBoost',
+        'vacHigh',
+        'noAutoPasses',
+        'twoPass'
+      ]).then((state) => {
+        if (!keepAlive) roomba.end();
         return state;
       });
   }
 
-  getRunningState(){
+  getRunningState() {
     return this.getState().cleanMissionStatus.cycle;
   }
 
-  getCarpetBoost(){
+  getCarpetBoost() {
     let carpetBoost = await this.getState().carpetBoost
     let vacHigh = await this.getState().vacHigh
-    if (carpetBoost && !vacHigh){
+    if (carpetBoost && !vacHigh) {
       return 'Auto';
-    } else if (!carpetBoost && vacHigh){
+    } else if (!carpetBoost && vacHigh) {
       return 'Performance';
-    } else if (!carpetBoost && !vacHigh){
+    } else if (!carpetBoost && !vacHigh) {
       return 'Eco';
     }
   }
-  getCleaningPasses(){
+  getCleaningPasses() {
     let noAutoPasses = await this.getState().noAutoPasses
     let twoPass = await this.getState().twoPass
-    if (!noAutoPasses && !twoPass){
+    if (!noAutoPasses && !twoPass) {
       return 'Auto'
-    }else if (noAutoPasses && twoPass){
+    } else if (noAutoPasses && twoPass) {
       return 'Performance'
-    }else if (noAutoPasses && !twoPass){
+    } else if (noAutoPasses && !twoPass) {
       return 'Eco'
     }
   }
 
-  start(room?){
+  start(room?) {
     await this.connect();
     if (room !== null) {
       await roomba.cleanRoom(room);
-    }else {
+    } else {
       await roomba.start();
     }
-    if(!keepAlive) roomba.end();
+    if (!keepAlive) roomba.end();
   }
 
-  stop(dock?: boolean){
+  stop(dock?: boolean) {
     await this.connect();
     await roomba.pause();
-    if (dock === true){
+    if (dock === true) {
       await roomba.dock();
     }
-    if(!keepAlive) roomba.end();
+    if (!keepAlive) roomba.end();
   }
 
-  identify(){
+  identify() {
     this.connect();
     roomba.find();
-    if(!keepAlive) roomba.end();
+    if (!keepAlive) roomba.end();
   }
 }
